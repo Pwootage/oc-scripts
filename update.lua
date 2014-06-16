e1f9f6c190fc02d046e43bb845dc44c4f044c26d
Pwootage <pwootage@gmail.com>
2014-06-15 23:22:47 -0600

    Fix update for real
    unfortunately requries absolute paths (read-only apparently)

--- update.lua
+++ update.lua
@@ -1,4 +1,7 @@
 --Auto update script (will DL this repo)
 
 local shell = require("shell")
-shell.execute("gitrepo Pwootage/oc-scripts ./oc-scripts")
+
+shell.execute("./gitrepo Pwootage/oc-scripts /disk/oc-scripts/")
+
+print("Updated cc-scripts (unless there was an error up there)")
